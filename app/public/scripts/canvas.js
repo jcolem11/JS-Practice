0d02fcd97f7ce75a32f00094cfca1310ad097efd
Joshua Coleman <joshua.coleman@DCM3WKFVH8L.local>
2016-05-17 22:33:01 -0400

    rain actually looks like rain; parallax

--- app/public/scripts/canvas.js
+++ app/public/scripts/canvas.js
@@ -1,8 +1,11 @@
 
 // Rain Drop Object
-function RainDrop (x,y) {
+function RainDrop (x,y,z) {
 	this.x = x;
 	this.y = y;
+	this.z = z;
+	this.height = getRandomArbitrary(25,80);
+	this.originalX = this.x;
 }
 
 var yPos = 0;
@@ -13,39 +16,55 @@ var elapsedTime = 0;
 
 // my functions
 function makeRain(){
-	var amount = Math.floor(Math.random() * (50 - 1) + 1);
-	console.log("amount added")
-	console.log(amount);
+	var amount = Math.floor(Math.random() * (20 - 1) + 1);
 	for (i=0; i < amount; i++){
 		var x = Math.floor(Math.random() * (2000 - 1) + 1);
 		var y = Math.floor(Math.random() * (200 - 1) + 1) * -1;
-		let newDrop = new RainDrop(x,y)
+		var z = Math.floor(Math.random() * (4 - 1) + 1);
+		let newDrop = new RainDrop(x,y,z)
 		drops.push(newDrop)
 	}
 }
+
+function getRandomArbitrary(min, max) {
+	return Math.floor(Math.random() * (max - min) + min);
+}
+
 //
 
 function setup() {
 	createCanvas(windowWidth,windowHeight);
-	background('#0D1257');
 	angleMode(DEGREES);
 	frameRate(60);
 	drops.push(drop);
 }
 
 function draw() {
-	background('#0D1257');
-	var resetTime = 0.2;
+	background(15, 19, 62);
+	var resetTime = 0.1;
 	elapsedTime++;
 	if ((elapsedTime/60) > resetTime){
 		makeRain()
 		elapsedTime = 0
 	}
+
 	for (i =0; i < drops.length; i++){
 		var d = drops[i];
-		d.y+= 10;
-		fill("#FE4F6E");
+		//	remove RainDrop
+		if (d.y+50 > windowHeight + 100){
+		var index = drops.indexOf(d);
+		if (index > -1) {
+			drops.splice(index, 1);
+			console.log("Removing drop");
+		 }
+	 }
+		d.y+= 40;
+		var midX = windowWidth/2;
+		d.x = (d.originalX + (midX - mouseX) * 1/(d.z));
+
+		fill(246, 246, 246,100/d.z);
 		noStroke();
-		rect(d.x,d.y,5,5);
+		var size = 5;
+		rect(d.x,d.y,size,80*(1/d.z));
 	}
 }
